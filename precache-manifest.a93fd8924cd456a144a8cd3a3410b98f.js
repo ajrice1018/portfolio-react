4629359d0b6ba51e1d62d52c4a87ffb0721e17e4
Andrew Rice <ajrice1018@gmail.com>
2020-09-30 16:20:57 -0700

    Updates

--- precache-manifest.a93fd8924cd456a144a8cd3a3410b98f.js
+++ precache-manifest.a93fd8924cd456a144a8cd3a3410b98f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b5335a27e1da7b630c1b657ab20d52d9",
+    "revision": "c4276e0384762ef84e47db6da9583556",
     "url": "/portfolio-react/index.html"
   },
   {
-    "revision": "3b50217eddd00140db71",
+    "revision": "49c8a7bd5ad4b786d0d9",
     "url": "/portfolio-react/static/css/main.a2b7b0b6.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio-react/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "3b50217eddd00140db71",
-    "url": "/portfolio-react/static/js/main.1ea26614.chunk.js"
+    "revision": "49c8a7bd5ad4b786d0d9",
+    "url": "/portfolio-react/static/js/main.c0a9d349.chunk.js"
   },
   {
     "revision": "11528ddda4154ce1e32b",
